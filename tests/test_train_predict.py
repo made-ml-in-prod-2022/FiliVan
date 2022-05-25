3002439614ce4864d07ae79bcb1f0e7dcbe474ba
FiliVanDev <filivandev@gmail.com>
2022-05-25 12:41:37 +0300

    change cli

--- tests/test_train_predict.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def test_pred_train():
-    exit_status_train = os.system("python ml_project/train.py")
-    exit_status_predict = os.system("python ml_project/predict.py")
-    assert exit_status_train == 0
-    assert exit_status_predict == 0
